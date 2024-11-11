f523365c07fec9a3f99309b572f0bf9f193aa2d0
omar karake <omar.karake@amalitechtraining.org>
2024-11-11 15:13:55 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from "./components/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, SidebarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
+  sidebarToggled: boolean = false;
+  toggleSidebar() {
+    this.sidebarToggled = !this.sidebarToggled;
+  }
   title = 'grids';
 }
